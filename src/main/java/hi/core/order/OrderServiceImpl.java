4a32045873a6e1e120b43ae3161156ba7474ab26
GoodDonkey <president20500@gmail.com>
2022-01-06 17:55:19 +0900

    Lombok: constructor automatic creation

--- src/main/java/hi/core/order/OrderServiceImpl.java
+++ src/main/java/hi/core/order/OrderServiceImpl.java
@@ -3,20 +3,16 @@ package hi.core.order;
 import hi.core.discount.DiscountPolicy;
 import hi.core.member.Member;
 import hi.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
