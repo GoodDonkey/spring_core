70891f030cb75191418afd35db5c817f56724fb2
GoodDonkey <president20500@gmail.com>
2022-01-06 17:55:19 +0900

    Component Scan: bean registration collision (recovered)

--- src/main/java/hi/core/AutoAppConfig.java
+++ src/main/java/hi/core/AutoAppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // 예제 Configuration 을 제외하기 위해 적용함.
-        basePackages = "hi.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
