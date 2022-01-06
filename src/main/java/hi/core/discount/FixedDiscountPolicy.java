023d12bdcc774a826afc4432ad621ae452f43ba8
GoodDonkey <president20500@gmail.com>
2022-01-06 17:55:19 +0900

    Component Scan: @Autowired and @Qualifier

--- src/main/java/hi/core/discount/FixedDiscountPolicy.java
+++ src/main/java/hi/core/discount/FixedDiscountPolicy.java
@@ -2,7 +2,11 @@ package hi.core.discount;
 
 import hi.core.member.Grade;
 import hi.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("FixedDiscountPolicy")
 public class FixedDiscountPolicy implements DiscountPolicy{
 
     private int discountFixedAmount = 1000; // 1000원 할인
