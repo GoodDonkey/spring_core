4c3d75c83dfb2a151934baaaec5f65b9e27f342c
GoodDonkey <president20500@gmail.com>
2022-01-10 13:00:32 +0900

    Scope: request scope bean not active until request

---
+++ src/main/java/hi/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hi.core.web;
+
+import hi.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody // 문자 바로 보내기
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+
+        logDemoService.logic("testId");
+        return "OK";
+
+    }
+}
