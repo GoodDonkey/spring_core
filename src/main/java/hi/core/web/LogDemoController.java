7277d1b9b73ed4b5afc1c93ae31b4189587c40b6
GoodDonkey <president20500@gmail.com>
2022-01-10 13:00:32 +0900

    Scope: ObjectProvider and request scope bean

--- src/main/java/hi/core/web/LogDemoController.java
+++ src/main/java/hi/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hi.core.web;
 
 import hi.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> provider;
 
     @RequestMapping("log-demo")
     @ResponseBody // 문자 바로 보내기
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = provider.getObject();
         myLogger.setRequestURL(requestURL);
-
         myLogger.log("controller test");
 
         logDemoService.logic("testId");
